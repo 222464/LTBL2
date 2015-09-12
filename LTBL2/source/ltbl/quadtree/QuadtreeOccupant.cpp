fc7479543a680a4d72bebc57106fbe7c927e82cd
Eric Laukien <ericlaukien@gmail.com>
2015-09-12 12:48:35 -0500

    Fixed removal bug

--- LTBL2/source/ltbl/quadtree/QuadtreeOccupant.cpp
+++ LTBL2/source/ltbl/quadtree/QuadtreeOccupant.cpp
@@ -2,6 +2,8 @@
 
 #include <ltbl/quadtree/QuadtreeNode.h>
 
+#include <ltbl/quadtree/Quadtree.h>
+
 #include <assert.h>
 
 using namespace ltbl;
@@ -9,9 +11,16 @@ using namespace ltbl;
 void QuadtreeOccupant::quadtreeUpdate() {
 	if (_pQuadtreeNode != nullptr)
 		_pQuadtreeNode->update(this);
+	else {
+		_pQuadtree->_outsideRoot.erase(this);
+
+		_pQuadtree->add(this);
+	}
 }
 
 void QuadtreeOccupant::quadtreeRemove() {
 	if (_pQuadtreeNode != nullptr)
 		_pQuadtreeNode->remove(this);
+	else
+		_pQuadtree->_outsideRoot.erase(this);
 }
